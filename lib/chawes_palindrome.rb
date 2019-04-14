c1022fbe1730db7a835eb2e93746eaaa00e5ef08
Conor Hawes <conor@launchpadlab.com>
2019-04-13 22:24:51 -0500

    Finishing up

--- lib/chawes_palindrome.rb
+++ lib/chawes_palindrome.rb
@@ -1,5 +1,13 @@
 require "chawes_palindrome/version"
 
-module ChawesPalindrome
-  # Your code goes here...
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+  
+  private
+  
+  def processed_content
+    scan(/[a-z]/i).join.downcase
+  end
 end
--- test/chawes_palindrome_test.rb
+++ test/chawes_palindrome_test.rb
@@ -5,7 +5,19 @@ class ChawesPalindromeTest < Minitest::Test
     refute_nil ::ChawesPalindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_not_a_palindrome
+    refute "apple".palindrome?
+  end
+  
+  def test_a_palindrome
+    assert "racecar".palindrome?
+  end
+  
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+  
+  def test_palindrome_with_punctuation
+    assert "Madam, I’m Adam.".palindrome?
   end
 end
